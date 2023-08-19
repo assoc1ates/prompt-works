d4ebcbb2b2e7dfcd7260055c9492b0c187721b9e
dOsan3 <>
2023-08-19 14:14:24 -0400

    Ensure that adding file path documentation is correct.

--- generate-prompts.sh
+++ generate-prompts.sh
@@ -1,18 +1,32 @@
+#!/bin/bash
+
 # Create the 'prompts' directory if it does not exist
+# Ensures that there is a directory to save the output files into.
 mkdir -p prompts
 
 # Read each line (i.e., each file path) from files.txt
+# Iterates over each line in the 'files.txt' file,
+# where each line is expected to be a path to a file.
 while IFS= read -r file_path
 do
     # Generate a unique output file name based on the input file name
-    # For example, if file_path is "$HOME/projects/filebot/Dockerfile",
-    # the output file will be "Dockerfile_output.txt"
+    # Constructs a new file name based on the name of the input file,
+    # appending '_output.txt' to create a unique name for the output file.
     output_file="$(basename "$file_path")_output.txt"
 
-    # Run the sed command using the current file_path and save the result
-    # in the uniquely named output file
-    # It first replaces $file_path with the actual file path,
-    # and then replaces $Document with the content of the file specified by file_path
-    sed -e "s|\$file_path|$file_path|g" -e "s/\$Document/$(sed 's/[&/]/\\&/g' <"$file_path" | sed ':a;N;$!ba;s/\n/\\n/g')/g" ./prompt.txt > "prompts/$output_file"
+    # Escape special characters in the file path
+    # Prepares the file path string so that it can be safely used in a sed command.
+    escaped_file_path=$(printf '%s\n' "$file_path" | sed 's:[\/&]:\\&:g;$!s/$/\\/')
+
+    # Replace '$filepath' placeholder with the actual file path
+    # The sed command reads the template from 'prompt.txt',
+    # replaces the placeholder '$filepath' with the actual path of the current file.
+    sed -e "s|\$filepath|$escaped_file_path|g" \
+        ./prompt.txt > "prompts/$output_file"
 
 done < files.txt
+
+# Script Description:
+# This script reads a list of file paths from 'files.txt',
+# processes each of these files to replace placeholders in a template text ('prompt.txt'),
+# and then writes the processed text to a new file in the 'prompts' directory.
