e6adc4f9977455882165eded2aa87d52f3be72fe
dOsan3 <>
2023-08-19 13:52:54 -0400

    Default llm endpoint and increase token max to upper limit.

--- modules/llm_model.py
+++ modules/llm_model.py
@@ -1,13 +1,12 @@
 import openai
 import openai_async
 
-async def generate_completion(prompt, model_name="gpt-3.5-turbo", max_tokens=256, temperature=0.9):
-    model_to_use = "gpt-4" if model_name == "gpt-3.5-turbo" else model_name
+async def generate_completion(prompt, model_name="gpt-3.5-turbo", max_tokens=4096, temperature=0.9):
     response = await openai_async.chat_complete(
         openai.api_key,
         timeout=60,
         payload={
-            "model": model_to_use,
+            "model": model_name,
             "messages": [
                 {"role": "system", "content": "You are a helpful assistant and great at guessing what files may have info based on loose summaries of the files."},
                 {"role": "user", "content": prompt},
