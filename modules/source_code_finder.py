3cc8074c9b278ee9f149aa2fe76bb98b010f9933
dOsan3 <>
2023-08-19 16:15:14 -0400

    Source code finder takes args for dir to traverse and where to put file with paths.

--- modules/source_code_finder.py
+++ modules/source_code_finder.py
@@ -1,33 +1,40 @@
 import os
 import re
 
-def traverse(dir_path):
+def traverse(dir_path, output_dir):
     """
     Recursively traverse through directories from the given directory path,
-    and yield the full paths of known source code file types.
-    Ignores directories named .git.
+    and write the full paths of known source code file types to a file named
+    'files.txt' in a specified directory. Ignores directories named .git.
 
     :param dir_path: The root directory path to start traversing from.
     :type dir_path: str
-    :yield: Full paths of known source code file types.
-    :rtype: str
+    :param output_dir: The directory where 'files.txt' will be created.
+    :type output_dir: str
     """
     # Regular expression pattern to match known source code file types
     source_code_pattern = re.compile(r".*\.(py|java|c|cpp|js|ts|go|rs|rb|php|swift|kt|scala|m|h|hpp|cs|sh|bash)$")
 
-    for root, dirs, files in os.walk(dir_path):
-        # Remove .git directory from traversal, if it exists
-        if '.git' in dirs:
-            dirs.remove('.git')
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
 
-        # Check each file in the current directory
-        for file_name in files:
-            if source_code_pattern.match(file_name):
-                # Full path of the source code file
-                full_path = os.path.join(root, file_name)
-                yield full_path
+    # Open the output file 'files.txt' in the specified directory
+    with open(os.path.join(output_dir, 'files.txt'), 'w') as outfile:
+        for root, dirs, files in os.walk(dir_path):
+            # Remove .git directory from traversal, if it exists
+            if '.git' in dirs:
+                dirs.remove('.git')
+
+            # Check each file in the current directory
+            for file_name in files:
+                if source_code_pattern.match(file_name):
+                    # Full path of the source code file
+                    full_path = os.path.join(root, file_name)
+                    # Write the full path of the source code file to 'files.txt'
+                    outfile.write(full_path + '\n')
 
 if __name__ == "__main__":
-    # Start traversing from the current directory, by default
-    for source_file in traverse("."):
-        print(source_file)
+    # Example usage
+    # Start traversing from the current directory
+    # Write the result to 'files.txt' in the directory named 'output'
+    traverse(".", ".")
